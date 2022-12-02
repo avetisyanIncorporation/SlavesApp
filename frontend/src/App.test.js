837bf248944beb8a32307e734e7f47e6d776d878
shibjuart <90416654+shibjuart@users.noreply.github.com>
2022-12-02 11:56:46 +0400

    break test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders slaves link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Slaves/i);
+  const linkElement = screen.getByText(/Slaves2/i);
   expect(linkElement).toBeInTheDocument();
 });
